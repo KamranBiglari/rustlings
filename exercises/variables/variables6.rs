7426e5272f2194587e61677cb132f8cd7ac70566
Kamran Biglari <102748921+KamranBiglari@users.noreply.github.com>
2023-09-01 23:18:24 +0000

    Done!

--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -3,9 +3,11 @@
 // Execute `rustlings hint variables6` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-const NUMBER = 3;
+const  NUMBER: i32 = 3;
 fn main() {
+    // change number to 6
+    let NUMBER_var = NUMBER;
+    // NUMBER = 6;
     println!("Number {}", NUMBER);
 }
